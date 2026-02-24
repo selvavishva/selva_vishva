c0b2e30f3c1176536b051a5498816ed4dded30fc
selva_vishva_M <selvavishva24@gmail.com>
2026-02-24 22:11:19 +0530

    palindrome in java

---
+++ Pali.java
@@ -0,0 +1,18 @@
+public class Pali {
+    public static void main(String[] args){
+        int num=121;
+        int temp = num;
+        int rev=0;
+        while (num>0){
+            int digit = num%10;
+            rev = rev*10+digit;
+            num=num/10;
+        }
+        if(temp==rev) {
+            System.out.println("palindrome");
+        }
+        else {
+            System.out.println("not palindrome");
+        }
+    }      
+}
