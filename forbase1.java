756328c3752d1642df3b415d750d1ead94694a8d
selva_vishva_M <selvavishva24@gmail.com>
2026-03-15 23:12:48 +0530

    For loop basic

---
+++ forbase1.java
@@ -0,0 +1,12 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class fot {
+    public static void main(String[] args) {
+        
+        for(int i=10;i>=1;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
