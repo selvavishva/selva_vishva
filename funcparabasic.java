047ef357ef2d45e6f82c70b7355d4b09b8db1f5e
selva_vishva_M <selvavishva24@gmail.com>
2026-03-31 18:43:08 +0530

    function paractice basic

---
+++ funcparabasic.java
@@ -0,0 +1,12 @@
+public class Funcparabasic {
+    
+    void choco(int money){
+        System.out.println(money);
+        System.out.println("chocolate purchased");
+    }
+    public static void main(String[] args) {
+        
+        Funcparabasic obj1 = new Funcparabasic();
+        obj1.choco(20);
+    }
+}
