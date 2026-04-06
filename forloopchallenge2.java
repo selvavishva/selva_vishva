a4da0e8a5381311489a8c74239d886e05e6db227
selva_vishva_M <selvavishva24@gmail.com>
2026-04-06 20:25:40 +0530

    for loop challenge2

---
+++ forloopchallenge2.java
@@ -0,0 +1,13 @@
+public class forloopchallenge2 {
+    public static void main(String[] args) {
+        
+        int count = 0;
+        for(int i=1;i<=10;i++){
+    
+            if (i%2==0) {
+                count = count+1;
+            }
+        }
+        System.out.println(count);
+    }
+}
