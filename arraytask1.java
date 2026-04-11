15bcd9c8c4e85767ce53c1f8b9a72028c858ab59
selva_vishva_M <selvavishva24@gmail.com>
2026-04-11 20:49:34 +0530

    Array task1

---
+++ arraytask1.java
@@ -0,0 +1,16 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class art {
+    public static void main (String[] args) {
+
+         int[] mark = {10,20,30,40,50,60,70,80,90,100};
+           
+      for(int i=1;i<=10;i++)
+        {
+       
+         System.out.println(mark[i]);
+    
+        }
+}
+}
